5f32176e4a6f9ab3b97f9bbcb43a787dec98d5db
Gwenyth Portillo-Wightman <gportill@usc.edu>
2018-11-09 20:24:56 -0800

    wip, able to graph multiple stocks in initial load but they look weird (straight)

--- src/chart.js
+++ src/chart.js
@@ -1,11 +1,12 @@
 var datatext;
-var stockSymbols = ['AAPL', 'FB'];
+var stockSymbols = ['AAPL', 'FB', 'GOOG'];
 var outData = [];
+var stockSeries = [];
 
-let chart = Highcharts.stockChart('container', {
+var myChart = Highcharts.stockChart('container', {
     chart: {
         events: {
-            // initial load for adding new series/ initial series will be seperatre function
+            // initial load for adding new series/ initial series will be seperate function
             // load is the per minute update
             load: function() {
                 var series = this.series;
@@ -19,38 +20,129 @@ let chart = Highcharts.stockChart('container', {
                 symbolString += stockSymbols[stockSymbols.length - 1];
                 console.log(symbolString);
 
-                setInterval(function() {
-                    $.ajax({
-                        method: 'GET',
-                        async: false,
-                        url: 'https://api.iextrading.com/1.0/stock/market/batch?symbols=' + symbolString + '&types=chart&range=1d&chartLast=1'
-                    })
-                    .done(function(results) {
-                        datatext = results;
-                    })
-                    .fail(function() {
-                        console.log("error");
-                    });
+                $.ajax({
+                    method: 'GET',
+                    async: false,
+                    url: 'https://api.iextrading.com/1.0/stock/market/batch?symbols=' + symbolString + '&types=chart&range=1d&chartLast=60'
+                })
+                .done(function(results) {
+                    datatext = results;
+                })
+                .fail(function() {
+                    console.log("error");
+                });
 
-                    stockSymbols = Object.keys(datatext);
+                stockSymbols = Object.keys(datatext);
 
-                    for (var symbolIndex = 0; symbolIndex < stockSymbols.length; symbolIndex++) {
-                        var stockName = stockSymbols[symbolIndex];
-                        var allStockInfo = datatext[stockName];
-                        var chartInfo = allStockInfo.chart;
+                for (symbolIndex = 0; symbolIndex < stockSymbols.length; symbolIndex++)
+                {
+                  var stockName = stockSymbols[symbolIndex];
+                  var allStockInfo = datatext[stockName];
+                  var chartInfo = allStockInfo.chart;
+                  var outData = [];
 
-                        var thing = chartInfo[0];
-                        var month = thing.date.substring(4,6);
-                        var mint = parseInt(month);
-                        mint--;
-                        var correctMonth = mint.toString();
-                        var datething = new Date(thing.date.substring(0,4), correctMonth, thing.date.substring(6,8), thing.minute.substring(0,2), thing.minute.substring(3,5), '00', '00');
-                        var millis = parseInt(Date.parse(datething.toISOString())) - (480*60000);
+                  for (var k = 0; k < 60; k++)
+                  {
+                    var thing = chartInfo[k];
+                    console.log(thing.date);
+                    var month = thing.date.substring(4, 6);
+                    var mint = parseInt(month);
+                    mint--;
+                    var correctMonth = mint.toString();
+                    var datething = new Date(thing.date.substring(0, 4),correctMonth, thing.date.substring(6, 8), thing.minute.substring(0, 2), thing.minute.substring(3, 5), '00', '00');
+                    var millis = parseInt(Date.parse(datething.toISOString())) - (480*60000);
+                    outData[k] = {
+                      x: millis,
+                      y: thing.average
+                    };
+                  }
+                  // series.push(outData);
 
-                        series[symbolIndex].addPoint([millis, thing.average], true, true);
-                    }
-                }, 60000);
+                  // myChart.addSeries({
+                  //  data: outData
+                  // });
+                  var jObj = JSON.parse(JSON.stringify(outData));
+
+                  /*
+                  $(function () {
+                      $('#container').highcharts({
+                          xAxis: {
+                              type: 'datetime',
+                              range: 3600 * 1000
+                          },
+                          series: [{
+                              data: jObj
+                          }]
+                      });
+                  });
+                  */
+                  stockSeries[symbolIndex] = outData;
+                }
+
+                // updateChart();
             }
         }
     }
 });
+
+for (var i = 0; i < stockSeries.length; i++)
+{
+  console.log("adding this series:");
+  console.log(stockSeries[i]);
+  myChart.addSeries({
+    name: stockSymbols[i],
+    data: stockSeries[i]
+  }, false);
+}
+myChart.redraw();
+
+/*
+function updateChart () {
+  var series = myChart.series;
+
+  var symbolString = "";
+
+  for (var j = 0; j < stockSymbols.length-1; j++)
+  {
+    symbolString += stockSymbols[j] + ",";
+  }
+  symbolString += stockSymbols[stockSymbols.length - 1];
+  console.log(symbolString);
+
+  setInterval(function() {
+      $.ajax({
+          method: 'GET',
+          async: false,
+          url: 'https://api.iextrading.com/1.0/stock/market/batch?symbols=' + symbolString + '&types=chart&range=1d&chartLast=1'
+      })
+      .done(function(results) {
+          datatext = results;
+      })
+      .fail(function() {
+          console.log("error");
+      });
+
+      stockSymbols = Object.keys(datatext);
+
+      for (var symbolIndex = 0; symbolIndex < stockSymbols.length; symbolIndex++) {
+          var stockName = stockSymbols[symbolIndex];
+          var allStockInfo = datatext[stockName];
+          var chartInfo = allStockInfo.chart;
+
+          var thing = chartInfo[0];
+          var month = thing.date.substring(4,6);
+          var mint = parseInt(month);
+          mint--;
+          var correctMonth = mint.toString();
+          var datething = new Date(thing.date.substring(0,4), correctMonth, thing.date.substring(6,8), thing.minute.substring(0,2), thing.minute.substring(3,5), '00', '00');
+          var millis = parseInt(Date.parse(datething.toISOString())) - (480*60000);
+
+          series[symbolIndex].addPoint([millis, thing.average], true, true);
+      }
+  }, 60000);
+}
+*/
+
+// function addStockLine() {
+//
+// }
