862e31e4d9415b5956a05db7a78c47ecf0749c52
Jiho Kim <captainhoji@naver.com>
2018-11-12 11:49:54 -0800

    Sell function complete
    Invest.php is still unstable, will be updated later today

--- src/myMoney.php
+++ src/myMoney.php
@@ -5,14 +5,6 @@
         $username = $_SESSION["username"];
         
         include 'databaseConnection.php';
-        
-        // Check if the username already exists
-        $sql = "SELECT * FROM Investments WHERE i_username=\"$username\";";
-        $results = $mysqli->query($sql);
-        if (!$results) {
-            echo "SQL ERROR: " . $mysqli->error;
-            exit();
-        }
 ?>
 <!DOCTYPE html>
 <html>
@@ -52,6 +44,24 @@
 </head>
 <body>
 <?php   
+        // get balance of this user
+        $sql = "SELECT * FROM Users WHERE u_username=\"$username\";";
+        $results = $mysqli->query($sql);
+        if (!$results) {
+            echo "SQL ERROR: " . $mysqli->error;
+            exit();
+        }
+?>
+<h3>My balance: $<?php echo $results->fetch_assoc()["u_balance"]/100.0;?></h3>
+<?php 
+
+        // get all investment data of this user
+        $sql = "SELECT * FROM Investments WHERE i_username=\"$username\" ORDER BY i_amount DESC;";
+        $results = $mysqli->query($sql);
+        if (!$results) {
+            echo "SQL ERROR: " . $mysqli->error;
+            exit();
+        }
         if ($results->num_rows >= 1) {
 ?>
 	       		<table><tr><th>Symbol</th><th>Amount</th></tr>
@@ -59,12 +69,13 @@
     	    while($row = $results->fetch_assoc()) {
 ?>
     	        <tr><td><?php echo $row["i_symbol"];?></td><td><?php echo $row["i_amount"];?></td>
-    	        <td><form action="invest.php?symbol=<?php echo $row["i_symbol"];?>" method="post">Buy <input type="number" name="buyAmount" min="0">
+    	        <td><form action="invest.php?symbol=<?php echo urlencode($row["i_symbol"]);?>" method="post">Buy <input type="number" name="buyAmount" min="1">
     	        <input type="submit" name="submit" value="Buy"></form><br />
-    	        <form action="invest.php?symbol=<?php echo $row["i_symbol"];?>" method="post">Sell <input type="number" name="buyAmount" min="0" max="<?php echo $row["i_amount"];?>">
-    	        <input type="submit" name="submit" value="Sell"></form>       
+    	        <form action="invest.php?symbol=<?php echo urlencode($row["i_symbol"]);?>" method="post">Sell <input type="number" name="sellAmount" min="1" max="<?php echo $row["i_amount"];?>">
+    	        <input type="submit" name="submit" value="Sell"></form></td></tr>
 <?php 
     	    }
+    	    echo "</table><hr>";
         }
         else {
             echo "<strong>Follow a stock to start investing!<strong>";
@@ -74,7 +85,15 @@
     else {
         header("Location: signin.php");
         exit();
-    }   
+    }
+    if (array_key_exists("status", $_REQUEST) && $_REQUEST["status"] == "success") {
+        echo "<strong>Transaction Successful</strong>";
+    }
+    else if (array_key_exists("lowBalance", $_REQUEST) && $_REQUEST["lowBalance"] == 1) {
+        echo "<strong>Not enough money to confirm transaction</strong>";
+    }
+    
+        
 ?>
 
 </body>
